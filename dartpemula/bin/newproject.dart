0282898e235318807506acbaf77210fc3ac53fec
Puteri Yudani <puteriyudani@gmail.com>
2021-03-16 13:00:45 +0700

    string input

--- dartpemula/bin/newproject.dart
+++ dartpemula/bin/newproject.dart
@@ -1,4 +1,12 @@
 // menampilkan 'Hello World!'
+// void main(List<String> arguments) {
+//   print('Hello world!');
+// }
+
+// String input
+import 'dart:io';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  String input = stdin.readLineSync();
+  print(input);
 }
