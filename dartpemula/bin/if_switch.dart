8a66315f2d37018b1bcf738ac5146ce068d7a9c0
Puteri Yudani <puteriyudani@gmail.com>
2021-03-16 13:17:29 +0700

    if else cara 4

--- dartpemula/bin/if_switch.dart
+++ dartpemula/bin/if_switch.dart
@@ -29,14 +29,24 @@
 //   print(output);
 // }
 
-// if else cara 3
+// // if else cara 3
+// import 'dart:io';
+
+// main(List<String> arguments) {
+//   int number = int.tryParse(stdin.readLineSync());
+//   String output;
+
+//   output = (number > 0) ? "positif" : "negatif atau nol";
+
+//   print(output);
+// }
+
+// // if else cara 4
 import 'dart:io';
 
 main(List<String> arguments) {
   int number = int.tryParse(stdin.readLineSync());
   String output;
 
-  output = (number > 0) ? "positif" : "negatif atau nol";
-
-  print(output);
+  print((number > 0) ? "positif" : "negatif atau nol");
 }
