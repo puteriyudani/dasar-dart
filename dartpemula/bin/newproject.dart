23ce72d6ab71b4db8d2a49116ac25e433cd44721
Puteri Yudani <puteriyudani@gmail.com>
2021-03-16 13:03:57 +0700

    double input

--- dartpemula/bin/newproject.dart
+++ dartpemula/bin/newproject.dart
@@ -12,10 +12,19 @@
 // }
 
 // Int Input
+// import 'dart:io';
+
+// void main(List<String> arguments) {
+//   String input = stdin.readLineSync();
+//   int number = int.tryParse(input);
+//   print(number + 10);
+// }
+
+// Double input
 import 'dart:io';
 
 void main(List<String> arguments) {
   String input = stdin.readLineSync();
-  int number = int.tryParse(input);
+  double number = double.tryParse(input);
   print(number + 10);
 }
