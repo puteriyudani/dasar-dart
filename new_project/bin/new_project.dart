a170bcdd023851b0127bfeba183b0623c68fe0ed
Puteri Yudani <puteriyudani@gmail.com>
2021-03-16 10:03:11 +0700

    menampilkan 'Hello World!'

--- new_project/bin/new_project.dart
+++ new_project/bin/new_project.dart
@@ -1,7 +1,3 @@
-import 'dart:io';
-
 void main(List<String> arguments) {
-  String input = stdin.readLineSync();
-  double number = double.tryParse(input);
-  print(number + 10);
+  print('Hello world!');
 }
