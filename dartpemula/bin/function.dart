c9e481ed89d1d7fcf29a1a50964565850ad9b82f
Puteri Yudani <puteriyudani@gmail.com>
2021-03-16 19:05:50 +0700

    optional parameter

--- dartpemula/bin/function.dart
+++ dartpemula/bin/function.dart
@@ -1,4 +1,4 @@
-// luas_segiempat
+// luas_segiempat (function)
 // import 'dart:io';
 
 // double luas_segiempat(double panjang, double lebar) {
@@ -18,18 +18,42 @@
 //   print(luas);
 // }
 
-// luas_segiempat cara 2
+// luas_segiempat cara 2 (function)
+// import 'dart:io';
+
+// double luas_segiempat(double panjang, double lebar) {
+//   return panjang * lebar;
+// }
+
+// main(List<String> arguments) {
+//   double p, l, luas;
+
+//   p = double.tryParse(stdin.readLineSync());
+//   l = double.tryParse(stdin.readLineSync());
+
+//   print(luas_segiempat(p, l));
+// }
+
+// optional parameter
 import 'dart:io';
 
-double luas_segiempat(double panjang, double lebar) {
-  return panjang * lebar;
+String say(String from, String message, {String to, String appName}) {
+  return from +
+      " say " +
+      message +
+      ((to != null) ? " to " + to : "") +
+      ((appName != null) ? " via " + appName : "");
+}
+
+String say2(String from, String message, [String to, String appName]) {
+  return from +
+      " say " +
+      message +
+      ((to != null) ? " to " + to : "") +
+      ((appName != null) ? " via " + appName : "");
 }
 
 main(List<String> arguments) {
-  double p, l, luas;
-
-  p = double.tryParse(stdin.readLineSync());
-  l = double.tryParse(stdin.readLineSync());
-
-  print(luas_segiempat(p, l));
+  print(say("Puteri", "Hello", to: "Mita", appName: "WhatsApp"));
+  print(say2("Mita", "Hello", "Puteri", "WhatsApp"));
 }
